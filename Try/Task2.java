c62ff1f559d95435c6d392f4c815627af7fd76d2
EdaDemii <eda-demirtas1@hotmail.com>
2022-07-07 21:57:47 -0400

    My coding task

---
+++ Try/Task2.java
@@ -0,0 +1,13 @@
+package Try;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int x = 10;
+        int z= 15;
+        if (x<z ){
+            System.out.println(z + " bigger than " +x);
+        }else{
+            System.out.println(x + "bigger than " +z);
+        }
+    }
+}
